8c41f390afcd95ba06b2cc7597b89161774275c4
asifrafiun17 <asifrafiun17@gmail.com>
2025-01-13 23:21:29 +0600

    :database

---
+++ includes/sms.sql
@@ -0,0 +1,666 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 10, 2025 at 06:17 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `image_name` varchar(255) NOT NULL,
+  `reg_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class_room`
+--
+
+CREATE TABLE `class_room` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `student_count` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam`
+--
+
+CREATE TABLE `exam` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam_range_grade`
+--
+
+CREATE TABLE `exam_range_grade` (
+  `id` int(11) NOT NULL,
+  `grade_id` int(11) NOT NULL,
+  `mark_range` varchar(255) NOT NULL,
+  `_from` int(11) NOT NULL,
+  `_to` int(11) NOT NULL,
+  `mark_grade` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam_timetable`
+--
+
+CREATE TABLE `exam_timetable` (
+  `id` int(11) NOT NULL,
+  `grade_id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  `day` varchar(255) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `classroom_id` int(11) NOT NULL,
+  `start_time` double(11,2) NOT NULL,
+  `end_time` double(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grade`
+--
+
+CREATE TABLE `grade` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `admission_fee` double(11,2) NOT NULL,
+  `hall_charge` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_message`
+--
+
+CREATE TABLE `group_message` (
+  `id` int(11) NOT NULL,
+  `conversation_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `sender_type` varchar(255) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `grade` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `main_notifications`
+--
+
+CREATE TABLE `main_notifications` (
+  `id` int(11) NOT NULL,
+  `notification_id` int(11) NOT NULL,
+  `_status` varchar(255) NOT NULL,
+  `year` year(4) NOT NULL,
+  `month` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `_isread` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parents`
+--
+
+CREATE TABLE `parents` (
+  `id` int(11) NOT NULL,
+  `my_son_index` bigint(11) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `i_name` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `image_name` varchar(255) NOT NULL,
+  `b_date` date NOT NULL,
+  `reg_date` date NOT NULL,
+  `reg_year` year(4) NOT NULL,
+  `reg_month` varchar(255) NOT NULL,
+  `_status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_notifications`
+--
+
+CREATE TABLE `payment_notifications` (
+  `id` int(11) NOT NULL,
+  `year` year(4) NOT NULL,
+  `month` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `_status` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `image_name` varchar(255) NOT NULL,
+  `b_date` date NOT NULL,
+  `_status` varchar(255) NOT NULL,
+  `reg_year` year(4) NOT NULL,
+  `reg_month` varchar(255) NOT NULL,
+  `reg_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_attendance`
+--
+
+CREATE TABLE `student_attendance` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `month` varchar(255) NOT NULL,
+  `year` year(4) NOT NULL,
+  `time` time NOT NULL,
+  `_status1` varchar(255) NOT NULL,
+  `_status2` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_exam`
+--
+
+CREATE TABLE `student_exam` (
+  `id` int(11) NOT NULL,
+  `grade_id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `marks` varchar(255) NOT NULL,
+  `year` year(4) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_grade`
+--
+
+CREATE TABLE `student_grade` (
+  `id` int(11) NOT NULL,
+  `grade_id` int(11) NOT NULL,
+  `year` year(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_payment`
+--
+
+CREATE TABLE `student_payment` (
+  `id` int(11) NOT NULL,
+  `year` year(4) NOT NULL,
+  `month` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `paid` double(11,2) NOT NULL,
+  `_status` varchar(255) NOT NULL,
+  `student_status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_payment_history`
+--
+
+CREATE TABLE `student_payment_history` (
+  `id` int(11) NOT NULL,
+  `grade_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `subject_fee` double(11,2) NOT NULL,
+  `subtotal` double(11,2) NOT NULL,
+  `_status` varchar(255) NOT NULL,
+  `month` varchar(255) NOT NULL,
+  `year` year(4) NOT NULL,
+  `date` date NOT NULL,
+  `invoice_number` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_subject`
+--
+
+CREATE TABLE `student_subject` (
+  `id` int(11) NOT NULL,
+  `_status` varchar(255) NOT NULL,
+  `sr_id` int(11) NOT NULL,
+  `year` int(11) NOT NULL,
+  `reg_month` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE `subject` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject_routing`
+--
+
+CREATE TABLE `subject_routing` (
+  `id` int(11) NOT NULL,
+  `grade_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `fee` double(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `image_name` varchar(255) NOT NULL,
+  `reg_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher_attendance`
+--
+
+CREATE TABLE `teacher_attendance` (
+  `id` int(11) NOT NULL,
+  `index_number` bigint(11) NOT NULL,
+  `date` date NOT NULL,
+  `month` varchar(255) NOT NULL,
+  `year` year(4) NOT NULL,
+  `time` time NOT NULL,
+  `_status1` varchar(255) NOT NULL,
+  `_status2` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher_salary`
+--
+
+CREATE TABLE `teacher_salary` (
+  `id` int(11) NOT NULL,
+  `month` varchar(255) NOT NULL,
+  `year` year(4) NOT NULL,
+  `date` date NOT NULL,
+  `paid` double(11,2) NOT NULL,
+  `_status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher_salary_history`
+--
+
+CREATE TABLE `teacher_salary_history` (
+  `id` int(11) NOT NULL,
+  `grade_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `subject_fee` double(11,2) NOT NULL,
+  `student_count` int(11) NOT NULL,
+  `hall_charge` int(11) NOT NULL,
+  `subtotal` double(11,2) NOT NULL,
+  `paid` double(11,2) NOT NULL,
+  `_status` varchar(255) NOT NULL,
+  `month` varchar(255) NOT NULL,
+  `year` year(4) NOT NULL,
+  `date` date NOT NULL,
+  `invoice_number` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timetable`
+--
+
+CREATE TABLE `timetable` (
+  `id` int(11) NOT NULL,
+  `grade_id` int(11) NOT NULL,
+  `day` varchar(255) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `classroom_id` int(11) NOT NULL,
+  `start_time` double(11,2) NOT NULL,
+  `end_time` double(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `type` enum('admin','teacher','student') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `class_room`
+--
+ALTER TABLE `class_room`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `exam`
+--
+ALTER TABLE `exam`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `exam_range_grade`
+--
+ALTER TABLE `exam_range_grade`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `exam_timetable`
+--
+ALTER TABLE `exam_timetable`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `grade`
+--
+ALTER TABLE `grade`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `group_message`
+--
+ALTER TABLE `group_message`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `main_notifications`
+--
+ALTER TABLE `main_notifications`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `parents`
+--
+ALTER TABLE `parents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payment_notifications`
+--
+ALTER TABLE `payment_notifications`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student_attendance`
+--
+ALTER TABLE `student_attendance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student_exam`
+--
+ALTER TABLE `student_exam`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student_grade`
+--
+ALTER TABLE `student_grade`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student_payment`
+--
+ALTER TABLE `student_payment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student_payment_history`
+--
+ALTER TABLE `student_payment_history`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student_subject`
+--
+ALTER TABLE `student_subject`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subject_routing`
+--
+ALTER TABLE `subject_routing`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `teacher_attendance`
+--
+ALTER TABLE `teacher_attendance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `teacher_salary`
+--
+ALTER TABLE `teacher_salary`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `teacher_salary_history`
+--
+ALTER TABLE `teacher_salary_history`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `timetable`
+--
+ALTER TABLE `timetable`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `payment_notifications`
+--
+ALTER TABLE `payment_notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student_attendance`
+--
+ALTER TABLE `student_attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student_exam`
+--
+ALTER TABLE `student_exam`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student_grade`
+--
+ALTER TABLE `student_grade`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student_payment`
+--
+ALTER TABLE `student_payment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student_payment_history`
+--
+ALTER TABLE `student_payment_history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student_subject`
+--
+ALTER TABLE `student_subject`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `subject_routing`
+--
+ALTER TABLE `subject_routing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `teacher_attendance`
+--
+ALTER TABLE `teacher_attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `teacher_salary`
+--
+ALTER TABLE `teacher_salary`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `teacher_salary_history`
+--
+ALTER TABLE `teacher_salary_history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `timetable`
+--
+ALTER TABLE `timetable`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
